70e6c70f56a05920fb3345098c3fae253f0afdbd
satish <satishpwebdev@gmail.com>
2025-01-13 00:06:40 +0530

    links error fixed

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const WelcomeScreen = () => {
   return (
     <>
@@ -25,14 +27,14 @@ const WelcomeScreen = () => {
                 device, anytime
               </p>
               <div className="mt-6 flex flex-wrap justify-center gap-y-4 gap-x-6">
-                <a
-                  href="/login"
+                <Link
+                  to="/login"
                   className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                 >
                   <span className="relative text-base font-semibold text-white">
                     Get started
                   </span>
-                </a>
+                </Link>
                 <a
                   href="#"
                   className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
